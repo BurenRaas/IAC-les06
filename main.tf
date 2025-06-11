babbaf520884251f2a07a86136e27e23e8f20312
Ruben <s1190828@student.windesheim.nl>
2025-06-11 17:18:17 +0000

    git workflow

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ data "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "2bvnet"
+  name                = "IAC-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
