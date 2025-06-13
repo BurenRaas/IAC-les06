52646a8c2ba5bc46334e9dfe2bbb7a86df32b694
Ruben <s1190828@student.windesheim.nl>
2025-06-13 05:48:14 +0000

    hopelijk werkt het nu eindelijk

--- main.tf
+++ main.tf
@@ -10,11 +10,98 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {}
+  subscription_id = "7c2cf771-1067-4e56-9047-4a218905ddaf"
+}
+
+#Variables in variables.tf
 provider "esxi" {
   esxi_hostname = var.esxi_hostname
+  esxi_hostport = var.esxi_hostport
+  esxi_hostssl  = var.esxi_hostssl
   esxi_username = var.esxi_username
   esxi_password = var.esxi_password
 }
+
+
+#resource group als data importeren zodat deze wel gebruikt kan worden, maar niet beheerd wordt door Terraform
+data "azurerm_resource_group" "rg" {
+  name = "s1190828"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "IAC-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "IAC-subnet"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  count               = 1
+  name                = "IAC-publicip-${count.index}"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Basic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  count               = 1
+  name                = "nic-${count.index}"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip[count.index].id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  count                           = 1
+  name                            = "AZ-IAC-vm-${count.index}"
+  resource_group_name             = data.azurerm_resource_group.rg.name
+  location                        = data.azurerm_resource_group.rg.location
+  size                            = "Standard_B2ats_v2"
+  admin_username                  = "student"
+  disable_password_authentication = true
+
+  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
+
+  admin_ssh_key {
+    username   = "student"
+    public_key = file("~/.ssh/iac.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(file("cloud-config.yml"))
+}
+
+output "azure_vm_ips" {
+  value = [for ip in azurerm_public_ip.pip : ip.ip_address]
+}
+
 
 #Web server op esx
 resource "esxi_guest" "webserver" {
@@ -51,8 +138,14 @@ echo "${ip} ansible_user=student ansible_ssh_private_key_file=~/.ssh/iac" >> inv
 ssh-keyscan -H ${ip} >> ~/.ssh/known_hosts
 %{endfor~}
 
+echo "${azurerm_public_ip.pip[0].ip_address} ansible_user=student ansible_ssh_private_key_file=~/.ssh/iac" >> inventory.ini
+ssh-keyscan -H ${azurerm_public_ip.pip[0].ip_address} >> ~/.ssh/known_hosts
 EOT
   }
 
-
+  depends_on = [
+    esxi_guest.webserver,
+    azurerm_linux_virtual_machine.vm,
+    azurerm_public_ip.pip
+  ]
 }
